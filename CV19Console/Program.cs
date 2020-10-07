a782b69b4bbc868580d959835ec00a36e3a6e37f
nubnist <44030910+nubnist@users.noreply.github.com>
2020-10-07 19:18:58 +0300

    Сделана основная инфраструктура проекта

--- CV19Console/Program.cs
+++ CV19Console/Program.cs
@@ -1,9 +1,22 @@
 ﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace CV19Console
 {
     class Program
     {
+        private const string data_url =
+            @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
+        private static async Task<Stream> GetDataStream()
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync(data_url, HttpCompletionOption.ResponseHeadersRead);
+
+            return await response.Content.ReadAsStreamAsync();
+        }
         static void Main(string[] args)
         {
 
