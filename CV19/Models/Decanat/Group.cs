9e841aa0f20d85205986d1ca847af0087416dc72
nubnist <44030910+nubnist@users.noreply.github.com>
2020-10-09 23:21:10 +0300

    Реализация фильтрации групп студентов в разметке и кода файла обработчиков событий окна

--- CV19/Models/Decanat/Group.cs
+++ CV19/Models/Decanat/Group.cs
@@ -5,6 +5,7 @@ namespace CV19.Models.Decanat
     internal class Group
     {
         public string Name { get; set; }
+        public string Description { get; set; }
         public IList<Student> Students { get; set; }
     }
 }
